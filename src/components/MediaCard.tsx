2e64d9ad73af17005abb41d9b63200977e68513b
uxfoxx <naveen.f.senanayake@gmail.com>
2026-05-04 00:26:11 +0530

    gg

--- src/components/MediaCard.tsx
+++ src/components/MediaCard.tsx
@@ -64,7 +64,7 @@ const MediaCard = ({
           transitionSpeed={900}
         >
           <div
-            className={`relative aspect-[4/5] overflow-hidden rounded-2xl shadow-sm group-hover:shadow-2xl group-hover:shadow-black/25 transition-shadow duration-500 border ${
+            className={`bg-white  relative aspect-[4/5] overflow-hidden rounded-2xl shadow-sm group-hover:shadow-2xl group-hover:shadow-black/25 transition-shadow duration-500 border ${
               isDark ? "border-white/10 bg-forest-deep/40" : "border-border/70  "
             }`}
           >
